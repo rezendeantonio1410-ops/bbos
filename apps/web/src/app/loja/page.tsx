a5520ab36346b72deb10b88038075a1f90f60aa2
rezendeantonio1410-ops <rezendeantonio1410@gmail.com>
2026-09-03 22:50:44 -0300

    style(storefront): switch redesigned home to sensory sales stylesheet

--- apps/web/src/app/loja/page.tsx
+++ apps/web/src/app/loja/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import styles from "./page.module.css";
+import styles from "./sales.module.css";
 import { productImages } from "./product-images";
 
 const products=[
